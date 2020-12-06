c76640124e55aeb6ac3e3b4cdd92048c665e7f52
Nat Welch <nat@timebyping.com>
2020-12-06 16:44:05 +0000

    add headtag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
 import { ThemeProvider, CSSReset } from '@chakra-ui/react';
+import Head from 'next/head';
 
 import customTheme from '../theme';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={customTheme}>
+      <Head>
+        <title>Kyoudai Industries</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <CSSReset />
       <Component {...pageProps} />
     </ThemeProvider>
