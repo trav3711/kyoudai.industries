33aff9f163f71d33c74ad06d28fcb1ae663f7682
Travis Welch <trav.cwelch@gmail.com>
2020-12-09 22:22:33 -0800

    added functionality

--- components/layout.js
+++ components/layout.js
@@ -7,25 +7,26 @@ const japanese_name = '兄弟事業'
 const english_name = 'Kyoudai Industries'
 export const siteTitle = 'Kyoudai Industries'
 
-export default function Layout({ home }) {
+export default function Layout({ children, home }) {
   return (
     <div className={styles.container}>
       <header className={styles.header}>
         {home ? (
           <>
-            <h1 className={utilStyles.heading2Xl}>{japanese_name}</h1>
-            <h1 className={utilStyles.heading2Xl}>{english_name}</h1>
+            <h1 className={utilStyles.heading3Xl}>{japanese_name}</h1>
+            <h1 className={utilStyles.headingXl}>{english_name}</h1>
           </>
         ) : (
           <>
-            <h2 className={utilStyles.headingLg}>
+            <h2 className={utilStyles.heading2Xl}>
               <Link href="/">
-                <a className={utilStyles.colorInherit}>{english_name}</a>
+                <a className={utilStyles.colorInherit}>{japanese_name}</a>
               </Link>
             </h2>
           </>
         )}
       </header>
+      <main>{ children }</main>
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+import indexStyles from '../styles/index.module.css'
 
 export default function Home() {
   return (
@@ -7,9 +10,28 @@ export default function Home() {
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <h1>This is the index page</h1>
-      <h1>This is the index page</h1>
-      <p>Some text</p>
+      <p className={utilStyles.headingLg}>A revolutionary contracting company formed by two brothers</p>
+      <div className={indexStyles.linkContainer}>
+        <Link href= './project_pages/metrics'>
+          <a className={utilStyles.linkLg}>Metrics</a>
+        </Link>
+      </div>
+      <div className={indexStyles.linkContainer}>
+        <Link href= 'https://github.com/trav3711/metrics'>
+          <a className={utilStyles.linkLg}>Newsroom</a>
+        </Link>
+      </div>
+      <div className={indexStyles.linkContainer}>
+        <Link href= 'https://github.com/trav3711/metrics'>
+          <a className={utilStyles.headingLg}>JWOTD</a>
+        </Link>
+      </div>
+      <div className={indexStyles.linkContainer}>
+        <Link href= 'https://github.com/trav3711/metrics'>
+          <a className={utilStyles.headingLg}>Contact Us</a>
+        </Link>
+      </div>
+
     </Layout>
   )
 }
