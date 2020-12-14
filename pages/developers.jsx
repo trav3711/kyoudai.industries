089745e35f20ffd9c29bae14b25dc3b26e17789b
Travis Welch <trav.cwelch@gmail.com>
2020-12-14 09:38:28 -0800

    fix merge issues

--- pages/developers.jsx
+++ pages/developers.jsx
@@ -18,25 +18,13 @@ export async function getStaticProps() {
 export default function developers({ allPostsData }) {
   return (
     <Layout>
-<<<<<<< HEAD
         {allPostsData.map(({ name, title, technologies, description }) => (
-          <div className={utilStyles.developer_container}>
+          <div key={name} className={utilStyles.developer_container}>
               <h1>{name}</h1>
               <h3 className={utilStyles.headingLg}>{technologies}</h3>
               <p>{description}</p>
           </div>
         ))}
-=======
-        <ul className={utilStyles.list}>
-            {allPostsData.map(({ name, title, technologies, description }) => (
-              <div key={name} className={utilStyles.developer_container}>
-                  <h1>{name}</h1>
-                  <h3 className={utilStyles.headingLg}>{technologies}</h3>
-                  <p>{description}</p>
-              </div>
-            ))}
-          </ul>
->>>>>>> 1b175f2427ad1fb5ba5fb6f61b3a80c4dfc6aff9
     </Layout>
   )
 };
