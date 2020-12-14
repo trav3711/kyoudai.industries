d8c17076598d7103df4f1841d6001220a41cb4a7
Travis Welch <trav.cwelch@gmail.com>
2020-12-13 16:50:38 -0800

    update gitignore

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,26 +1,18 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import Header, { siteTitle } from '../components/header'
+
+import Layout, { siteTitle } from '../components/layout'
+
 import utilStyles from '../styles/utils.module.css'
 import indexStyles from '../styles/index.module.css'
 
-const Home() {
+function Home() {
   return (
-    <Header home>
+    <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <p className={utilStyles.headingLg}>
-        Welcome! Kyoudai Industries was started with the goal of enabling junior software developers the chance
-        to enter the freelance market by removing the classic experience barriers.
-      </p>
-      <p>
-        Of course you may ask, isn't a good amount of experience necessary to make a productive freelance engineer?
-        The answer is yes, but at Kyoudai Industries we believe that every engineer has value,
-        and want to be the initial stepping stone that enables younger engineers to thrive in the modern tech workplace.
-      </p>
-
-    </Header>
+    </Layout>
   )
 }
 
